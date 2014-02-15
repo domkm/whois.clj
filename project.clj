95679e900c43c25eb7874552739bdba9674014b2
Dom Kiva-Meyer <git@domkm.com>
2014-02-15 12:59:29 -0800

    update project config

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject domkm.whois "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject com.domkm/whois "0.0.1-SNAPSHOT"
+  :description "A Clojure wrapper for the Ruby WHOIS library (http://ruby-whois.org/) running on JRuby."
+  :url "https://github.com/DomKM/whois.clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
