cd5387a6202b9a97585b106abe3ce94c48aeb0e9
Dom Kiva-Meyer <git@domkm.com>
2014-02-16 21:05:39 -0800

    add deps

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/DomKM/whois.clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.jruby/jruby-complete "1.7.10"]
+                 [zweikopf "1.0.0" :exclusions [org.jruby/jruby-complete]]])
