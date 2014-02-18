ef4a15810508c2f61a4d382d7a8e509f846e1cac
Dom Kiva-Meyer <git@domkm.com>
2014-02-17 19:41:30 -0800

    fix bug

--- src/domkm/whois.clj
+++ src/domkm/whois.clj
@@ -1,5 +1,6 @@
 (ns domkm.whois
-  (:require [zweikopf.multi :as rb :refer [call-ruby clojurize Clojurize ruby-eval]])
+  (:require [clojure.java.io :as io]
+            [zweikopf.multi :as rb :refer [call-ruby clojurize Clojurize ruby-eval]])
   (:import [org.jruby.embed ScriptingContainer LocalContextScope]
            [org.jruby RubyStruct]))
 
@@ -9,7 +10,7 @@
     (persistent!
       (reduce (fn [acc key]
                 (assoc! acc
-                        (clojurize key ruby)
+                        (keyword (clojurize key ruby))
                         (clojurize (call-ruby ruby this "[]" key) ruby)))
               (transient {})
               (.members this)))))
@@ -20,11 +21,13 @@
 ;; ScriptingContainer container = new ScriptingContainer()
 ;; container.getProvider().getRubyInstanceConfig().setCompileMode(CompileMode.JIT)
 
-(ruby-eval ruby "ENV['GEM_PATH']='gems'; ENV['GEM_HOME']='gems'; require 'whois'")
+(ruby-eval ruby "ENV['GEM_PATH']='gems';
+                 ENV['GEM_HOME']='gems';
+                 require 'whois'")
 
 (def ^:private whois-gem (ruby-eval ruby "Whois"))
 
 (defn whois [^String q]
-  (let [record (call-ruby ruby whois-gem q)]
+  (let [record (call-ruby ruby whois-gem :lookup q)]
     {:parts (clojurize (call-ruby ruby record :parts) ruby)
      :properties (clojurize (call-ruby ruby record :properties) ruby)}))
